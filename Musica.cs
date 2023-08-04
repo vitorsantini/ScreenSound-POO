a4725e784a5098526c8558073d67b779e25e89d7
Vitor Zanchini Santini <115574189+vitorsantini@users.noreply.github.com>
2023-08-04 17:11:50 -0300

    Criando Repositorio do Projeto

---
+++ Musica.cs
@@ -0,0 +1,31 @@
+﻿using System.Globalization;
+
+class Musica
+{
+    public Musica(Banda artista, string nome)
+    {
+        Artista = artista;
+        Nome = nome;
+    }
+
+    public string Nome { get; }
+    public Banda Artista { get; }
+    public int Duracao { get; set; }
+    public bool Disponivel { get; set; }
+    public string DescricaoResumida => $" A música {Nome} pertence à banda {Artista}";
+    public Genero Genero { get; set; }
+
+    public void ExibirFichaTecnica()
+    {
+        Console.WriteLine($"Nome: {Nome}");
+        Console.WriteLine($"Artista: {Artista.Nome}");
+        Console.WriteLine($"Duração: {Duracao} segundos");
+        if (Disponivel)
+        {
+            Console.WriteLine("Disponivel no plano\n");
+        } else
+        {
+            Console.WriteLine("Adiquira o plano Plus\n");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿Banda evanescence = new Banda("Evanescence");
+
+Album albumDoEvanescence = new Album("Fallen");
+
+
+Musica musica1 = new Musica(evanescence, "Taking Over Me")
+{
+    Duracao = 228,
+    Disponivel = true,
+ 
+};
+
+Musica musica2 = new Musica(evanescence, "My Immortal")
+{
+    Duracao = 262,
+    Disponivel = false,
+    
+};
+
+
+albumDoEvanescence.AdicionarMusica(musica1);
+albumDoEvanescence.AdicionarMusica(musica2);
+evanescence.AdicionarAlbum(albumDoEvanescence);
+
+musica1.ExibirFichaTecnica();
+musica2.ExibirFichaTecnica();
+albumDoEvanescence.ExibirMusicasDoAlbum();
+evanescence.ExibirDiscografia();
